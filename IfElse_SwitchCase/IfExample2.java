8b9519166e4faeba494e9412a95dc6f76fa09f70
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-02-28 09:35:37 +0530

    Create IfExample2.java

---
+++ IfElse_SwitchCase/IfExample2.java
@@ -0,0 +1,17 @@
+package IfElse_SwitchCase;
+
+public class IfExample2 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        if(a>b){
+            System.out.println("a greater than b");
+        }
+        if(a<b){
+            System.out.println("a less than b");
+        }
+        if(a==b){
+            System.out.println("a is equal to b");
+        }
+    }
+}
