6c20b073e9b934255b3478237f2c8ef381a7b411
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-02-28 11:32:58 +0530

    Create Sum.java

---
+++ Question/Sum.java
@@ -0,0 +1,12 @@
+package Question;
+import java.util.Scanner;
+public class Sum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("enter the numner :");
+        int a=sc.nextInt();
+        System.out.println("enter the number :");
+        int b=sc.nextInt();
+        System.out.println( "sum :"+(a+b));
+    }
+}
