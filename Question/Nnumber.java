e128ad6af26f008787fbc80be337df1c5c122451
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-02-28 11:41:08 +0530

    Create Nnumber.java

---
+++ Question/Nnumber.java
@@ -0,0 +1,12 @@
+package Question;
+import java.util.Scanner ;
+public class Nnumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("enter the number :");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+            System.out.println(i);
+        
+    }
+}
