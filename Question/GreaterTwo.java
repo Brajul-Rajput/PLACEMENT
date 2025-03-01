313e8f12cb6a51ef1b5b2035d2a66ae5a7547869
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-03-01 10:32:13 +0530

    Create GreaterTwo.java

---
+++ Question/GreaterTwo.java
@@ -0,0 +1,14 @@
+package Question;
+
+public class GreaterTwo {
+    public static void main(String[] args) {
+        int a=20;
+        int b=23;
+        if(a>b){
+            System.out.println("a is greater than b");
+        }
+        else{
+            System.out.println("b is greater than a");
+        }
+    }
+}
