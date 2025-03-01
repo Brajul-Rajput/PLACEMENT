e0b12a8d1f2777044b3f7c4d11beaa767e0fb4e3
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-03-01 10:32:26 +0530

    Create ReverseArray.java

---
+++ Question/ReverseArray.java
@@ -0,0 +1,16 @@
+package Question;
+import java.util.*;
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int []arr={10,20,30,40,50};
+        for(int b:arr){
+            System.out.print(b+"  ");
+        }
+        System.out.println(" ");
+        System.out.println("Reverse now :");
+        for(int i=4;i>=0;i--){
+            System.out.print(arr[i]+"  ");
+        }
+    }
+}
