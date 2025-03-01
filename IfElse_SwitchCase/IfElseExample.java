3a50441ab27d3a5c32b08df7e4363023bb269f1d
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-03-01 10:31:04 +0530

    Create IfElseExample.java

---
+++ IfElse_SwitchCase/IfElseExample.java
@@ -0,0 +1,13 @@
+package IfElse_SwitchCase;
+
+public class IfElseExample {
+    public static void main(String[] args) {
+        int age=20;
+        if(age>=18){
+            System.out.println("you are eligible for vote !!!");
+        }
+        else{
+            System.out.println("you are teenagers ");
+        }
+    }
+}
