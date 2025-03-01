84fbefc5c300477f4d2ab62b07d38900fda3a0c5
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-03-01 10:31:10 +0530

    Create MaxThreeVar.java

---
+++ IfElse_SwitchCase/MaxThreeVar.java
@@ -0,0 +1,17 @@
+package IfElse_SwitchCase;
+
+public class MaxThreeVar {
+    public static void main(String[] args) {
+        int a=20;
+        int b=45;
+        int max=0;
+        if(a>b){
+            max=a;
+            System.out.println(max);
+        }
+        else{
+            max=b;
+            System.out.println(max);
+        }
+    }
+}
