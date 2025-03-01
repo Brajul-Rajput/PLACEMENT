205f0b31cebd955346f128dc5a9eb797fcdc1934
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-03-01 10:32:33 +0530

    Create StringArray.java

---
+++ Question/StringArray.java
@@ -0,0 +1,10 @@
+package Question;
+import java.util.*;
+public class StringArray {
+    public static void main(String[] args) {
+        String arr[]={"brajul","surjit","niko","chork"};
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.asList(arr));
+        System.out.println(Arrays.deepToString(arr));
+    }
+}
