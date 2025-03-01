2c388aa3a29d197d74005df8a9131711ec1e3c18
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-03-01 10:31:18 +0530

    Create MaxTwo.java

---
+++ IfElse_SwitchCase/MaxTwo.java
@@ -0,0 +1,14 @@
+package IfElse_SwitchCase;
+
+public class MaxTwo {
+   public static void main(String[] args) {
+    int a=10;
+    int b=20;
+    if(a>b){
+        System.out.println("A is greater than B");
+    }
+    else{
+        System.out.println("B  is greater than A");
+    }
+   } 
+}
