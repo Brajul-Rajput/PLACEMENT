c64aa9d11f3220f8e685ab8c5af1d2457727e6fb
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-03-05 08:22:35 +0530

    Create Equal.java

---
+++ Question/Equal.java
@@ -0,0 +1,12 @@
+package Question;
+
+import java.util.Arrays;
+
+public class Equal {
+    public static void main(String[] args) {
+        int []arr1={1,2,3,4,5,6 };
+        int []arr2={1,2,3,4,5,6 ,7};
+        boolean flag=Arrays.equals(arr1,arr2);
+        System.out.println(flag);
+}
+}
