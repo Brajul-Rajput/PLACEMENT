0fe1dc1adb345de6bc4a130aed893c6ae5a6fbf6
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-03-06 16:26:52 +0530

    Create AverageArray.java

---
+++ Array_java/AverageArray.java
@@ -0,0 +1,24 @@
+package Array_java;
+
+import java.util.Scanner;
+
+public class AverageArray {
+ 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter the size of the array :");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        int sum=0;
+        System.out.println("Enter the element of the array :");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<size;i++){
+            sum+=arr[i];
+        }
+        System.out.println("The sum of the array is : "+sum);
+        System.out.println("The average of the array is : "+sum/size);
+    }
+}
