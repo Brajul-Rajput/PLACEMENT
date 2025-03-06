13130949d61a01457f81d8152152ff042f491bfc
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-03-06 16:28:55 +0530

    Create NumberSystem.java

---
+++ Question/NumberSystem.java
@@ -0,0 +1,17 @@
+package Question;
+public class NumberSystem{
+    public static void main(String[] args) {
+        int a=32;
+        int b=0b010010;
+        int c=0x20A;
+        int d=032;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        
+
+        
+    }
+}
+
