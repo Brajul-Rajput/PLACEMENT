992d5562cf582667a55ba1fb819c1b07050f4cc5
Brajul Singh <132092432+Brajul-Rajput@users.noreply.github.com>
2025-03-09 22:44:47 +0530

    Create LeetCode07.java

---
+++ DSA/LeetCode07.java
@@ -0,0 +1,16 @@
+package DSA;
+
+public class LeetCode07 {
+    public static void main(String[] args) {
+        double x=2.00000;
+        int n=10;
+        myPow(x,n);
+            }
+        
+        
+                    public static double myPow(double x, int n) {
+                (double)Math.pow(x,n);
+            }
+        }
+    
+
